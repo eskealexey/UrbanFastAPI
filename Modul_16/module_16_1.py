e1bebdc8351aaf091cee56429076e849e0a91f1a
alexey <eskealexey@mail.ru>
2024-08-05 21:52:53 +0300

    Домашнее задание по теме "Основы Fast Api и маршрутизация

--- Modul_16/module_16_1.py
+++ Modul_16/module_16_1.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 # import uvicorn
 
-
 app = FastAPI()
 
 
@@ -14,12 +13,12 @@ async def welcome() -> str:
 async def adminka() -> str:
     return "Вы вошли как администратор"
 
-@app.get("/user")
-async def user_full(username: str = "Alex", age: int = 50) -> str:
-    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
-
 
 @app.get("/user/{user_id}")
 async def user(user_id) -> str:
     return f"Вы вошли как пользователь № {user_id}"
 
+
+@app.get("/user")
+async def user_full(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
